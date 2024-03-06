1cc934f2379c95b006790c83630ad473a9747022
Omr50 <omrab4@gmail.com>
2024-03-06 15:51:11 -0500

    Added Home Page

--- app/main/routes.py
+++ app/main/routes.py
@@ -5,3 +5,17 @@ from app.main import bp
 @bp.route('/')
 def index():
     return render_template("index.html")
+
+@bp.route('/navbar')
+def navbar():
+    return render_template("navbar.html")
+
+@bp.route('/form')
+def form():
+    return render_template("form.html")
+
+# return json for the next generated question.
+# @bp.route('/form_question')
+# def formQuestion():
+
+
