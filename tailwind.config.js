833f7deca78cd5bf82fc8b92137adbcd1e90422c
prince-ao <prince.addo@cix.csi.cuny.edu>
2024-03-11 14:38:09 -0400

    animation transition added

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,19 @@ module.exports = {
       },
       minHeight: {
         'main': 'calc(100vh - 160px)'
+      },
+      animation: {
+        appear: 'appear 1s ease-in-out forwards'
+      },
+      keyframes: {
+        appear: {
+          'from': {
+            opacity: 0
+          },
+          'to': {
+            opacity: 1
+          }
+        }
       }
     },
   },
