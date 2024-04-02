33b372a96f83447fae33b29875fa1cacda3235bd
prince-ao <prince.addo@cix.csi.cuny.edu>
2024-04-02 07:38:07 -0400

    no more javascript!

--- app/chat/routes.py
+++ app/chat/routes.py
@@ -10,7 +10,7 @@ def chatbot():
 
 @chat_bp.post('/ai-response')
 def ai_response():
-    # userInput = request.form['userInput']
+    userInput = request.form['userInput']
 
     time.sleep(1)
-    return "<p class='hidden-response'>There are many different kinds of animals that live in China. Tigers and leopards are animals that live in China's forests in the north. In the jungles, monkeys swing in the trees and elephants walk through the brush.</p>"
+    return "There are many different kinds of animals that live in China. Tigers and leopards are animals that live in China's forests in the north. In the jungles, monkeys swing in the trees and elephants walk through the brush."
